0bba3e333c8b5c412afe71782746b72e5045d976
chintu46306 <crg46306@gmail.com>
2024-04-19 17:19:36 +0530

    added : turn-highlight(Current turn)

--- tic-tac-toe/src/components/Grid/Grid.jsx
+++ tic-tac-toe/src/components/Grid/Grid.jsx
@@ -4,10 +4,13 @@ import "./Grid.css";
 
 function Grid({numberOfCards}){
     const [board, setBoard] = useState(Array(numberOfCards).fill(""));
-
+    const [turn, setTurn] = useState(true);  //true => o, flase => x
     return(
-        <div className="grid">
+        <div className="grid-wrapper">
+            <h1 className= "turn-highlight">Current turn: {(turn)? 'o' : 'x'}</h1>
+            <div className="grid">
             {board.map((el,idx)=> <Card key= {idx} />)}
+        </div>
         </div>
     );
 
